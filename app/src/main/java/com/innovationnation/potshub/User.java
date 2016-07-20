3554b9f3f42e8402c07171626fd45bfaedc2bae9
Kisa (Chris) Ayano <kisa.ayano@icloud.com>
2016-07-20 13:42:32 -0400

    Beginning actual Android App and backend
    Signed-off-by: Kisa (Chris) Ayano <kisa.ayano@icloud.com>

---
+++ app/src/main/java/com/innovationnation/potshub/User.java
@@ -0,0 +1,12 @@
+package com.innovationnation.potshub;
+
+/**
+ * Created by kisaayano on 20/07/16.
+ */
+public class User {
+    private String name, nickname, username, potsType;
+    private int y, m, d;
+    public User(String newName, String newNickname, String newUsername, String newPotsType, int by, int bm, int bd){
+
+    }
+}
