7d9399fa25425e4a1458115e4a3a20dcd7aaa854
Kisa (Chris) Ayano <kisa.ayano@icloud.com>
2016-07-20 15:13:23 -0400

    Backend
    Signed-off-by: Kisa (Chris) Ayano <kisa.ayano@icloud.com>

---
+++ app/src/main/java/com/innovationnation/potshub/PData.java
@@ -0,0 +1,8 @@
+package com.innovationnation.potshub;
+
+/**
+ * Created by kisaayano on 20/07/16.
+ */
+public class PData {
+    private String[] types = {"Neuropathic", "Hyperandrogenic", "Partial Dysautonomic"};
+}
--- app/src/main/java/com/innovationnation/potshub/User.java
+++ app/src/main/java/com/innovationnation/potshub/User.java
@@ -1,12 +1,77 @@
 package com.innovationnation.potshub;
 
+import java.util.Date;
+
 /**
  * Created by kisaayano on 20/07/16.
  */
 public class User {
     private String name, nickname, username, potsType;
     private int y, m, d;
-    public User(String newName, String newNickname, String newUsername, String newPotsType, int by, int bm, int bd){
+    private Date birthDate;
 
+    public User(String newName, String newNickname, String newUsername, String newPotsType, Date newBDate) {
+        name = newName;
+        nickname = newNickname;
+        username = newUsername;
+        potsType = newPotsType;
+        birthDate = newBDate;
+
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPotsType() {
+        return potsType;
+    }
+
+    public void setPotsType(String potsType) {
+        this.potsType = potsType;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
     }
 }
