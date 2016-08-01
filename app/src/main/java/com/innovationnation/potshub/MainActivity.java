20d93c541f719efe52991b9cd668f3bdd5fe4d54
Jennings Zhang <jenningszh@gmail.com>
2016-08-01 10:00:59 -0400

    wow another commit removed user

--- app/src/main/java/com/innovationnation/potshub/MainActivity.java
+++ app/src/main/java/com/innovationnation/potshub/MainActivity.java
@@ -73,15 +73,8 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(this, PrefActivity.class);
-            startActivity(i);
-            return true;
-        }
+        // as you specify a parent activity in AndroidManifest.xml
+        startActivity(new Intent(this, PrefActivity.class));
         updateTextFields();
         return super.onOptionsItemSelected(item);
     }
