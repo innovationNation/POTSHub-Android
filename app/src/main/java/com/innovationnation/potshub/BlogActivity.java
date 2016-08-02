795d32f570a5409677d06bf501a24a121daeefeb
Kisa (Chris) Ayano <kisa.ayano@icloud.com>
2016-08-02 10:58:35 -0400

    Many things changed
    Signed-off-by: Kisa (Chris) Ayano <kisa.ayano@icloud.com>

--- app/src/main/java/com/innovationnation/potshub/BlogActivity.java
+++ app/src/main/java/com/innovationnation/potshub/BlogActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BlogActivity extends AppCompatActivity {
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,9 @@ public class BlogActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("Social");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
